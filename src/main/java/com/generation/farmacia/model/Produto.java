5bcf6d96d352d4ce95fbc2df3192d5919d591c4f
Lucas Pinheiro <lucaspinheiros2@hotmail.com>
2023-05-23 17:52:05 -0300

    Correção de erros na camada Model

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -4,14 +4,18 @@ import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+@Entity
+@Table(name = "tb_produtos")
 public class Produto {
 	
 	@Id
